76ef5a20a5735e57a60c41229ad7e45a9edf475a
이단비 <102282703+hidanbeing@users.noreply.github.com>
2026-02-10 19:34:34 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.04 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -1,13 +1,11 @@
 class Solution {
     public int solution(int[] num_list) {
-        int answer = 0;
-        
+        int answer = -1;
         for (int i=0;i<num_list.length;i++){
             if (num_list[i]<0){
                 return i;
             }
         }
-     return -1;
-        
+        return answer;
     }
 }
