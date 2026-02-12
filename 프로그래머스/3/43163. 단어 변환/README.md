e16285282150feff300c1afa886704b550a89402
이단비 <102282703+hidanbeing@users.noreply.github.com>
2026-02-12 23:39:38 +0900

    [level 3] Title: 단어 변환, Time: 0.40 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
 
 ### 성능 요약
 
-메모리: 71.9 MB, 시간: 0.63 ms
+메모리: 82.8 MB, 시간: 0.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 14일 20:15:32
+2026년 02월 12일 23:39:36
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,59 +1,59 @@
+
 import java.util.*;
 
+// begin	target	words	return
+// "hit"	"cog"	["hot", "dot", "dog", "lot", "log", "cog"]	4
+// "hit"	"cog"	["hot", "dot", "dog", "lot", "log"]	0
+
 class Solution {
-    public int solution(String begin, String target, String[] words) {
-        boolean isTarget = false;
-        for (String word : words){
-            if (word.equals(target)) isTarget = true;
-        }
-        if (isTarget==false) return 0;
+
+    int[] visited;
     
-        int n = words.length;
-        
-        Queue<Node> q = new LinkedList<>();
-        boolean[] visited = new boolean[n];
-        
-        for (int i=0;i<n;i++){
-            if (check(words[i], begin)){
-                q.offer(new Node(words[i], 1));
-                visited[i]=true;
-            }
-        }
-        
+   int bfs(int start, String[] words, String target){
+        Queue<Integer> q = new LinkedList();
+        q.add(start);
+        visited[start]=1;
         while (!q.isEmpty()){
-            Node node = q.poll();
-            if (node.str.equals(target)){
-                return node.depth;
+            int p=q.poll();
+            if (words[p].equals(target)){
+                return visited[p];
             }
-            for (int i=0;i<n;i++){
-                if (!visited[i] && check(words[i], node.str)){
-                    q.offer(new Node(words[i], node.depth+1));
-                    visited[i]=true;    
+            for (int i=0;i<words.length;i++){
+                if (i==start) continue;
+                if (diff1(words[i], words[p]) && visited[i]==0){
+                    q.add(i);
+                    visited[i]=visited[p]+1;
+                }
+            }
+        
+        }
+        return -1;
+    }
+
+    boolean diff1(String a, String b){
+        int n=0;
+        for (int i=0;i<a.length();i++){
+            if (a.charAt(i)!=b.charAt(i)) n++;
+        }
+        if (n==1) return true;
+        else return false;
+    }
+    
+    public int solution(String begin, String target, String[] words) {
+
+        int m=100000000;
+        boolean tf=false;
+        for (int i = 0; i < words.length; i++) {
+            if (diff1(words[i], begin)) {
+                visited = new int[words.length];
+                int p= bfs(i, words,target);
+                if (p!=-1&&p<m){
+                    m=p;
+                    tf=true;
                 }
             }
         }
-        int answer = 0;
-        return answer;
-    }
-    
-    
-    
-    public boolean check(String str1, String str2){
-        int c = 0;
-        for (int i=0;i< str1.length();i++){
-            if (str1.charAt(i) != str2.charAt(i)) c++;
-        }
-        if (c == 1) return true;
-        else return false;
-    } 
-    
-    
-    class Node{
-        String str;
-        int depth;
-        Node(String s, int n){
-            str = s;
-            depth = n;
-        }
+        if (!tf) return 0;
+        return m;
     }
 }
