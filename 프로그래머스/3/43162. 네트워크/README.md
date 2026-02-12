12f4c23977a1e7057993e2b1ae1032db6808d4a3
이단비 <102282703+hidanbeing@users.noreply.github.com>
2026-02-12 23:03:58 +0900

    [level 3] Title: 네트워크, Time: 0.20 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 81.8 MB, 시간: 0.25 ms
+메모리: 76.8 MB, 시간: 0.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 14일 20:25:28
+2026년 02월 12일 23:03:55
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,40 @@
+import java.util.*;
+
+// n	computers	                        return
+// 3	[[1, 1, 0], [1, 1, 0], [0, 0, 1]]	2
+// 3	[[1, 1, 0], [1, 1, 1], [0, 1, 1]]	1
+
 class Solution {
-    boolean[] visited = new boolean[201];
-    
+    boolean[] visited;
+    int nn;
+    void dfs(int start, int size,int[][] computers){
+        visited[start]=true;
+        for (int i=0;i<size;i++){
+            if (i!=start){
+                if (visited[i]==false && computers[start][i]==1){
+                    dfs(i, size,computers);
+                }
+            }
+            
+        }
+    }
+
     public int solution(int n, int[][] computers) {
-        int answer = 0;
+        visited=new boolean[n];
+        nn=0;
+
         for (int i=0;i<n;i++){
             if (visited[i]==false){
                 dfs(i,n,computers);
-                answer++;
-            }        
-        }
-        return answer;
-    }
-    
-    // dfs 이용
-    public void dfs(int v,int n, int[][] computers){
-        visited[v] = true;
-        
-        for (int i=0;i<n;i++){
-            if (visited[i]==true) continue;
-            if (computers[v][i]==1){
-                 dfs(i,n,computers);
+                nn++;
+                
             }
+            
+            // System.out.println(nn);
         }
+
+
+        int answer = 0;
+        return nn;
     }
 }
