b47b1a7cd77c17f7bb00870aa16a9ce9a6f9d737
이단비 <102282703+hidanbeing@users.noreply.github.com>
2026-03-05 14:37:38 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 108.87 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,23 +1,25 @@
 import java.util.*;
-
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> pp = new HashMap<>();
-        
-        for (String str: participant){
-            pp.put(str, pp.getOrDefault(str,0)+1);
-        }
-        
-        for (String str: completion){
-            pp.put(str, pp.get(str)-1);
-        }
-        
-        String answer = "";
-        
-        for (String str: participant){
-            if (pp.get(str)!=0) answer=str;
-        }
-        
-        return answer;
+	Map<String, Integer> map = new HashMap<>();
+    	
+    	for (String s: participant) {
+    		map.put(s,map.getOrDefault(s,0)+1);
+    	}
+    	
+    	for (String s: completion) {
+    		map.put(s,map.get(s)-1);
+    	}
+    	
+    	String ans="";
+    	
+    	for (String s: participant) {
+    		if (map.get(s)>0) {
+    			ans=s;
+    		}
+    	}
+    	
+    	
+    	return ans;
     }
 }
